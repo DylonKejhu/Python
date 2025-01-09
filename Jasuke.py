1a033ec769ae25b27c162285724f7e87e3e12a87
Dylon <dillonmanyo@gmail.com>
2025-01-09 11:40:03 +0800

    Adding Cek Resep Jasuke

---
+++ Jasuke.py
@@ -0,0 +1,61 @@
+def Jasuke(Jagung, SKM, Keju, Gula, Garam, Mentega):
+    print(f"\nBahan dalam produk anda :\n"
+          f"{Jagung} buah Jagung\n"
+          f"{SKM} ml SKM\n"
+          f"{Keju} gram Keju\n"
+          f"{Gula} sdm Gula\n"
+          f"{Garam} sdt Garam\n"
+          f"{Mentega} sdt Mentega\n")
+    
+    if 2 <= Jagung <= 3:
+        print("Jagungnya Pas !")
+    elif Jagung < 2:
+        print("jagungnya Kurang !")
+    elif Jagung > 3:
+        print("jagungnya Kelebihan ! !")
+         
+    if 150 <= SKM <= 250:
+        print("SKM Pas !")
+    elif SKM < 150:
+        print("SKM Kurang !")
+    elif SKM > 250:
+        print("SKM Kelebihan ! !")
+    
+    if 75 <= Keju <= 125:
+        print("Keju Pas !")
+    elif Keju < 75:
+        print("Keju Kurang !")
+    elif Keju > 125:
+        print("Keju Kelebihan ! !")
+        
+    if 2 <= Gula <= 3:
+        print("Gula Pas !")
+    elif Gula < 2:
+        print("Gula Kurang !")
+    elif Gula > 3:
+        print("Gula Kelebihan ! !")
+        
+    if 1 <= Garam <= 2:
+        print("Garam Pas !")
+    elif Garam < 1:
+        print("Garam Kurang !")
+    elif Garam > 2:
+        print("Garam Kelebihan ! !")
+    
+    if Mentega == 1:
+        print("Mentega Pas !")
+    elif Mentega < 1:
+        print("Mentega Kurang !")
+    elif Mentega > 1:
+        print("Mentega Kelebihan ! !")
+        
+    
+    
+A = int(input("Masukan Jagung (Buah) : "))
+B = int(input("Masukan SKM (ml) : "))
+C = int(input("Masukan Keju (gram) : "))
+D = int(input("Masukan Gula (sdm) : "))
+E = int(input("Masukan Garam (sdt) : "))
+F = int(input("Masukan Mentega (sdt) : "))
+
+Jasuke(A, B, C, D, E, F)
--- Tek_Kotek.py
+++ Tek_Kotek.py
@@ -2,18 +2,18 @@ import time
 
 jumlah_ayam = ["satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh"]
 
-print("\nTek kotek kotek kotek\n"
-          "Anak ayam turun berkotek\n"
-          "Tek kotek kotek kotek\n"
-          "Anak ayam turun berkotek")
-
-for ayam in reversed(jumlah_ayam):
-    print(f"\nAnak ayam turunlah {ayam}")
-    if ayam != "satu":
-        print(f"Mati satu tinggal {jumlah_ayam[jumlah_ayam.index(ayam) - 1]}")
-    else:
-        print("Mati satu tinggal induknya\n")
-    time.sleep(0.5)
+# print("\nTek kotek kotek kotek\n"
+#           "Anak ayam turun berkotek\n"
+#           "Tek kotek kotek kotek\n"
+#           "Anak ayam turun berkotek")
+#
+# for ayam in reversed(jumlah_ayam):
+#     print(f"\nAnak ayam turunlah {ayam}")
+#     if ayam != "satu":
+#         print(f"Mati satu tinggal {jumlah_ayam[jumlah_ayam.index(ayam) - 1]}")
+#     else:
+#         print("Mati satu tinggal induknya\n")
+#     time.sleep(0.5)
 
 # jumlah = 10
 # for ayam in range(jumlah):
@@ -24,3 +24,17 @@ for ayam in reversed(jumlah_ayam):
 #     elif jumlah == 0:
 #         print("Mati Satu tinggal Induknya\n")
 #     time.sleep(0.5)
+
+for ayam in reversed(jumlah_ayam):
+    # print(jumlah_ayam.index(ayam))
+    if jumlah_ayam.index(ayam) % 2 != 0:
+        print("\nTek kotek kotek kotek\n"
+          "Anak ayam turun berkotek\n"
+          "Tek kotek kotek kotek\n"
+          "Anak ayam turun berkotek")
+    print(f"\nAnak ayam turunlah {ayam}")
+    if ayam != "satu":
+        print(f"Mati satu tinggal {jumlah_ayam[jumlah_ayam.index(ayam) - 1]}")
+    else:
+        print("Mati satu tinggal induknya\n")
+    time.sleep(0.5)
